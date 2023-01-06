e57e506f3ffb9cc5f804da03786e23edc16238a6
Ruibin Liu <ruibinliuphd@gmail.com>
2023-01-06 14:19:34 -0500

    block->category

--- pdbx2df/read_pdbx.py
+++ pdbx2df/read_pdbx.py
@@ -5,49 +5,49 @@ import pandas as pd  # type: ignore
 from .split_line import split_line
 
 
-def read_pdbx(pdbx_file: str, block_names: list = None) -> dict:
+def read_pdbx(pdbx_file: str, category_names: list = None) -> dict:
     """
-    Read a pdbx file block into Pandas DataFrame.
+    Read a pdbx file categories into Pandas DataFrame.
 
     Args:
         pdbx_file (str): file name for a PDBx file.
-        block_names (list|None): a list of names for the blocks in a PDBx file that need to be read.
+        category_names (list|None): a list of names for the categories in a PDBx file that need to be read.
 
     Returns:
-        A dict of {block_name: pd.DataFrame of the info belongs to the block}
+        A dict of {category_name: pd.DataFrame of the info belongs to the category}
     """
     data: dict[str, pd.DataFrame] = {}
-    if not block_names:
+    if not category_names:
         return data
     with open(pdbx_file, "r") as pf:
         line = pf.readline()  # title line
         _ = line.strip().split("_")[1]
-        processing_block = False
+        processing_category = False
         while line:
             if line[0] == "#":
-                processing_block = True
-                loop_block = False
-                block_lines = ""
-                block_cols = []
-                block_dict = defaultdict(list)
-                line = pf.readline()  # block first line
+                processing_category = True
+                loop_category = False
+                category_lines = ""
+                category_cols = []
+                category_dict = defaultdict(list)
+                line = pf.readline()  # category first line
                 if not line:
                     break
                 if line == "loop_\n":
-                    loop_block = True
+                    loop_category = True
                     line = pf.readline()
                 in_multiple_line = (
                     False  # lines quoted by ';', they need to be specially treated
                 )
                 while line[0] != "#":
                     if line[0] == "_":
-                        block_name, block_nth_col = (
+                        category_name, category_nth_col = (
                             line.strip().split(" ")[0].split(".")
                         )
-                        block_cols.append(block_nth_col)
+                        category_cols.append(category_nth_col)
                     if (
-                        block_name in block_names or "all" in block_names
-                    ):  # in a required block
+                        category_name in category_names or "all" in category_names
+                    ):  # in a required category
                         if line[0] == ";":
                             in_multiple_line = True  # a ';' quoted record begins
                             line = line.replace(
@@ -63,22 +63,22 @@ def read_pdbx(pdbx_file: str, block_names: list = None) -> dict:
                             line = line.replace(
                                 '"', "'"
                             )  # to avoid conflict with the above action
-                        block_lines += line
+                        category_lines += line
                     line = pf.readline()
                     if not line:
                         raise ValueError(
                             f"{pdbx_file} not normally ended. Download it again?"
                         )
-                if "all" not in block_names and block_name not in block_names:
+                if "all" not in category_names and category_name not in category_names:
                     continue
-                block_lines = block_lines.replace(
+                category_lines = category_lines.replace(
                     "\n", " "
                 )  # cannot rely on newline to parse anyway.
-                records = split_line(block_lines)
-                if not loop_block:
-                    if len(records) != 2 * len(block_cols):
+                records = split_line(category_lines)
+                if not loop_category:
+                    if len(records) != 2 * len(category_cols):
                         raise ValueError(
-                            f"{pdbx_file} block {block_name} has irregular contents: {records}"
+                            f"{pdbx_file} category {category_name} has irregular contents: {records}"
                         )
                     for i, rec in enumerate(records):
                         if i % 2 == 0:
@@ -88,36 +88,36 @@ def read_pdbx(pdbx_file: str, block_names: list = None) -> dict:
                                 "pdbx_seq_one_letter_code_can",
                             ]:
                                 records[i + 1] = records[i + 1].replace(" ", "")
-                            block_dict[rec.split(".")[1]] = [records[i + 1]]
+                            category_dict[rec.split(".")[1]] = [records[i + 1]]
                 else:
                     if (
-                        records[len(block_cols)][0] == "_"
-                        or records[len(block_cols) - 1][0] != "_"
-                        or not len(records) % len(block_cols) == 0
+                        records[len(category_cols)][0] == "_"
+                        or records[len(category_cols) - 1][0] != "_"
+                        or not len(records) % len(category_cols) == 0
                     ):
                         raise ValueError(
-                            f"""{pdbx_file} block {block_name} has irregular contents:
-                            {len(records)} for {len(block_cols)}"""
+                            f"""{pdbx_file} category {category_name} has irregular contents:
+                            {len(records)} for {len(category_cols)}"""
                         )
-                    record_data = records[len(block_cols) :]
+                    record_data = records[len(category_cols) :]
                     for i, rd in enumerate(record_data):
-                        col = i % len(block_cols)
-                        if block_cols[col] in [
+                        col = i % len(category_cols)
+                        if category_cols[col] in [
                             "pdbx_seq_one_letter_code",
                             "pdbx_seq_one_letter_code_can",
                         ]:
                             rd = rd.replace(
                                 " ", ""
                             )  # delete the gaps in the one_letter_code
-                        block_dict[block_cols[col]].append(rd)
-                data[block_name] = pd.DataFrame(block_dict)
-                for bn in block_names:
+                        category_dict[category_cols[col]].append(rd)
+                data[category_name] = pd.DataFrame(category_dict)
+                for bn in category_names:
                     if bn not in data.keys():
-                        break  # at least one required_block is not processed, continue the main while loop
+                        break  # at least one required_category is not processed, continue the main while loop
                 else:
-                    break  # all required_blocks are processed, break the main while loop
+                    break  # all required_categorys are processed, break the main while loop
 
-            if not processing_block:
+            if not processing_category:
                 line = pf.readline()
 
     return data
