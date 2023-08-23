b56eb23643cb272b101e06db46a870a2eb11d4b1
Ruibin Liu <ruibinliuphd@gmail.com>
2023-08-23 09:33:16 -0400

    format

--- pdbx2df/read_pdbx.py
+++ pdbx2df/read_pdbx.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from collections import defaultdict
 
 import pandas as pd  # type: ignore
--- pdbx2df/write_pdb.py
+++ pdbx2df/write_pdb.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from datetime import date
 
 import numpy as np  # type: ignore
