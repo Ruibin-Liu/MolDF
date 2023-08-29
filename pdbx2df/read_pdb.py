3eead5552786831f5155d34144a9b5b3e4059e37
Ruibin Liu <ruibinliuphd@gmail.com>
2023-08-29 16:37:26 +0000

    NMR models

--- pdbx2df/read_pdb.py
+++ pdbx2df/read_pdb.py
@@ -8,6 +8,7 @@ import pandas as pd  # type: ignore
 
 IMPLEMENTED_PDB_CATS = ["_atom_site"]
 ATOM_SITE = ["ATOM  ", "HETATM", "TER   "]
+NMR_MDL = ["NUMMDL", "MODEL ", "ENDMDL"]
 
 
 def read_pdb(
@@ -41,9 +42,12 @@ def read_pdb(
         data[category_name] = pd.DataFrame()
 
     atom_site_lines = ""
-    atom_lines = ""
-    hetatm_lines = ""
-    with open(pdb_file, "r") as pf:
+    n_nmr_modes = 0
+    n_model_lines = 0
+    n_endmdl_lines = 0
+    nmr_model = None
+    atom_site_line_nmr_model = []
+    with open(pdb_file, "r", encoding="utf-8") as pf:
         for line in pf:
             if "_atom_site" in category_names and line[0:3] == "TER":
                 # This is for pdbfixer fixed PDB files whose TER lines are non standard.
@@ -58,14 +62,34 @@ def read_pdb(
                     stacklevel=2,
                 )
                 continue
+            if "_atom_site" in category_names and line[0:6] in NMR_MDL:
+                if line[0:6] == "MODEL ":
+                    n_model_lines += 1
+                    nmr_model = int(line[6:].strip())
+                elif line[0:6] == "ENDMDL":
+                    n_endmdl_lines += 1
+                else:
+                    n_nmr_modes = int(line[6:].strip())
             if "_atom_site" in category_names and line[0:6] in ATOM_SITE:
                 atom_site_lines += line
-                if line[0:4] == "ATOM" or line[0:3] == "TER":
-                    atom_lines += line
-                elif line[0:6] == "HETATM":
-                    hetatm_lines += line
+                if nmr_model is not None:
+                    atom_site_line_nmr_model.append(nmr_model)
+        if n_model_lines != n_endmdl_lines:
+            raise ValueError(
+                f"NMR records MODEL ({n_model_lines} lines) and ENDMDL ({n_endmdl_lines} lines) not matched."
+            )
+        if n_nmr_modes != n_model_lines:
+            warnings.warn(
+                f"The NUMMDL says {n_nmr_modes} NMR models, but only {n_model_lines} found.",
+                RuntimeWarning,
+                stacklevel=2,
+            )
     if "_atom_site" in category_names:
+        atom_site_buffer = io.StringIO()
+        atom_site_buffer.writelines(atom_site_lines)
+        atom_site_buffer.seek(0)
         if allow_chimera:
+            col_widths = [5, 6, 1, 4, 1, 4, 1, 4, 1, 3, 8, 8, 8, 6, 6, 6, 4, 2, 2]
             col_names = [
                 "record_name",
                 "atom_number",
@@ -73,7 +97,7 @@ def read_pdb(
                 "atom_name",
                 "alt_loc",
                 "residue_name",
-                # "blank_2", # possibly not in Chimera
+                # "blank_2", # removed to be compatible with Chimera-formatted PDBs
                 "chain_id",
                 "residue_number",
                 "insertion",
@@ -88,55 +112,8 @@ def read_pdb(
                 "element_symbol",
                 "charge",
             ]
-            # 'ATOM' records
-            atom_buffer = io.StringIO()
-            atom_buffer.writelines(atom_lines)
-            atom_buffer.seek(0)
-            atom_col_widths = [5, 6, 1, 4, 1, 4, 1, 4, 1, 3, 8, 8, 8, 6, 6, 6, 4, 2, 2]
-            assert sum(atom_col_widths) == 80
-            assert len(atom_col_widths) == len(col_names)
-            df_atom = pd.read_fwf(atom_buffer, widths=atom_col_widths, names=col_names)
-            atom_buffer.close()
-            # 'HETATM' records
-            hetatm_buffer = io.StringIO()
-            hetatm_buffer.writelines(hetatm_lines)
-            hetatm_buffer.seek(0)
-            hetatm_col_widths = [
-                6,
-                5,
-                1,
-                4,
-                1,
-                4,
-                1,
-                4,
-                1,
-                3,
-                8,
-                8,
-                8,
-                6,
-                6,
-                6,
-                4,
-                2,
-                2,
-            ]
-            assert sum(hetatm_col_widths) == 80
-            assert len(hetatm_col_widths) == len(col_names)
-            df_hetatm = pd.read_fwf(
-                hetatm_buffer, widths=hetatm_col_widths, names=col_names
-            )
-            hetatm_buffer.close()
-
-            df_atom_site = pd.concat([df_atom, df_hetatm], axis=0)
-            df_atom_site.reset_index(drop=True, inplace=True)
         else:
-            atom_site_buffer = io.StringIO()
-            atom_site_buffer.writelines(atom_site_lines)
-            atom_site_buffer.seek(0)
             col_widths = [6, 5, 1, 4, 1, 3, 1, 1, 4, 1, 3, 8, 8, 8, 6, 6, 6, 4, 2, 2]
-            assert sum(col_widths) == 80
             col_names = [
                 "record_name",
                 "atom_number",
@@ -159,14 +136,18 @@ def read_pdb(
                 "element_symbol",
                 "charge",
             ]
-            assert len(col_widths) == len(col_names)
-            df_atom_site = pd.read_fwf(
-                atom_site_buffer, widths=col_widths, names=col_names
-            )
-            atom_site_buffer.close()
+        assert sum(col_widths) == 80
+        assert len(col_widths) == len(col_names)
+        df_atom_site = pd.read_fwf(atom_site_buffer, widths=col_widths, names=col_names)
+        atom_site_buffer.close()
 
         col_names = [col_name for col_name in col_names if "blank" not in col_name]
         df_atom_site = df_atom_site[col_names]
+        if allow_chimera:
+            df_atom_site = _fix_chimera(df_atom_site)
+
+        if atom_site_line_nmr_model:
+            df_atom_site["nmr_model"] = atom_site_line_nmr_model
 
         str_names = [
             "atom_name",
@@ -182,3 +163,31 @@ def read_pdb(
         data["_atom_site"] = df_atom_site
 
     return data
+
+
+def _fix_chimera(df_atom_site: pd.DataFrame) -> pd.DataFrame:
+    """Internal function to fix the 'record_name' and 'atom_number' columns in processing Chimera formatted PDBs.
+
+    Args:
+        df_atom_site (pd.DataFrame): original PDB dataframe.
+
+    Returns:
+        pd.DataFrame: updated PDB dataframe
+    """  # noqa
+    original_record_names = list(df_atom_site.record_name)
+    record_names = [
+        record_name if record_name in ["ATOM", "TER"] else record_name + "M"
+        for record_name in original_record_names
+    ]
+    df_atom_site["record_name"] = record_names
+
+    print("TEST1", df_atom_site["atom_number"].dtypes)
+    if df_atom_site["atom_number"].dtypes != "int64":
+        original_atom_numbers = list(df_atom_site.atom_number)
+        atom_numbers = [
+            int(atom_number.lstrip("M").strip())
+            for atom_number in original_atom_numbers
+        ]
+        df_atom_site["atom_number"] = atom_numbers
+
+    return df_atom_site
--- tests/test_read_pdb.py
+++ tests/test_read_pdb.py
@@ -13,13 +13,33 @@ def test_read_pdb():
     """
     Test read_pdb function
     """
+
+    # non-NMR PDB
     file_path = [CFD, "test_files", "5K9I.pdb"]
     test_file = f"{os.sep}".join(file_path)
-
-    # Basic one category read
     pdb = read_pdb(test_file, category_names=["_atom_site"])
 
     compare_to = [CFD, "test_files", "5K9I.csv"]
+    compare_to = f"{os.sep}".join(compare_to)
+    df_expected = pd.read_csv(compare_to)
+    str_names = [
+        "atom_name",
+        "alt_loc",
+        "residue_name",
+        "chain_id",
+        "insertion",
+        "segment_id",
+        "element_symbol",
+    ]
+    df_expected[str_names] = df_expected[str_names].fillna("")
+    pd.testing.assert_frame_equal(pdb["_atom_site"], df_expected)
+
+    # NMR PDB
+    file_path = [CFD, "test_files", "1G03.pdb"]
+    test_file = f"{os.sep}".join(file_path)
+    pdb = read_pdb(test_file, category_names=["_atom_site"])
+
+    compare_to = [CFD, "test_files", "1G03.csv"]
     compare_to = f"{os.sep}".join(compare_to)
     df_expected = pd.read_csv(compare_to)
     str_names = [
