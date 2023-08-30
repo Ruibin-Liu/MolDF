edbb9974d6fc9b3ed2ab1faf1486750602fc2833
Ruibin Liu <ruibinliuphd@gmail.com>
2023-08-30 17:33:08 +0000

    rewrote to make it much faster

--- pdbx2df/read_pdb.py
+++ pdbx2df/read_pdb.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
-import io
 import os
 import warnings
 
 import pandas as pd  # type: ignore
 
 IMPLEMENTED_PDB_CATS = ["_atom_site"]
-ATOM_SITE = ["ATOM  ", "HETATM", "TER   "]
-NMR_MDL = ["NUMMDL", "MODEL ", "ENDMDL"]
+ATOM_SITE = ("ATOM", "HETATM", "TER")
+NMR_MDL = ("NUMMDL", "MODEL", "ENDMDL")
 
 
 def read_pdb(
@@ -41,152 +40,122 @@ def read_pdb(
             raise ValueError(f"Only {implemented} are implented for the PDB format.")
         data[category_name] = pd.DataFrame()
 
-    atom_site_lines = ""
-    n_nmr_modes = 0
+    num_nmr_models = 0
     n_model_lines = 0
-    n_endmdl_lines = 0
     nmr_model = None
-    atom_site_line_nmr_model = []
+    n_endmdl_lines = 0
+    atom_site_rows = []
+
     with open(pdb_file, "r", encoding="utf-8") as pf:
         for line in pf:
-            if "_atom_site" in category_names and line[0:3] == "TER":
-                # This is for pdbfixer fixed PDB files whose TER lines are non standard.
-                # This condition section can be removed if the above problem is fixed in pdbfixer.
-                line = line.rstrip()
-                line_len = len(line)
-                line = line + " " * (80 - line_len) + "\n"
-            elif len(line) != 81:
-                warnings.warn(
-                    f"Line {line} has non-standard length {len(line) - 1}, not 80; skipped",
-                    RuntimeWarning,
-                    stacklevel=2,
-                )
-                continue
-            if "_atom_site" in category_names and line[0:6] in NMR_MDL:
-                if line[0:6] == "MODEL ":
+            if line.startswith(NMR_MDL):
+                if line.startswith("NUMMDL"):
+                    num_nmr_models = int(line[6:].strip())
+                elif line.startswith("MODEL"):
                     n_model_lines += 1
                     nmr_model = int(line[6:].strip())
-                elif line[0:6] == "ENDMDL":
+                elif line.startswith("ENDMDL"):
                     n_endmdl_lines += 1
-                else:
-                    n_nmr_modes = int(line[6:].strip())
-            if "_atom_site" in category_names and line[0:6] in ATOM_SITE:
-                atom_site_lines += line
-                if nmr_model is not None:
-                    atom_site_line_nmr_model.append(nmr_model)
-        if n_model_lines != n_endmdl_lines:
-            raise ValueError(
-                f"NMR records MODEL ({n_model_lines} lines) and ENDMDL ({n_endmdl_lines} lines) not matched."
-            )
-        if n_nmr_modes != n_model_lines:
-            warnings.warn(
-                f"The NUMMDL says {n_nmr_modes} NMR models, but only {n_model_lines} found.",
-                RuntimeWarning,
-                stacklevel=2,
-            )
-    if "_atom_site" in category_names:
-        atom_site_buffer = io.StringIO()
-        atom_site_buffer.writelines(atom_site_lines)
-        atom_site_buffer.seek(0)
-        if allow_chimera:
-            col_widths = [5, 6, 1, 4, 1, 4, 1, 4, 1, 3, 8, 8, 8, 6, 6, 6, 4, 2, 2]
-            col_names = [
-                "record_name",
-                "atom_number",
-                "blank_1",
-                "atom_name",
-                "alt_loc",
-                "residue_name",
-                # "blank_2", # removed to be compatible with Chimera-formatted PDBs
-                "chain_id",
-                "residue_number",
-                "insertion",
-                "blank_3",
-                "x_coord",
-                "y_coord",
-                "z_coord",
-                "occupancy",
-                "b_factor",
-                "blank_4",
-                "segment_id",
-                "element_symbol",
-                "charge",
-            ]
-        else:
-            col_widths = [6, 5, 1, 4, 1, 3, 1, 1, 4, 1, 3, 8, 8, 8, 6, 6, 6, 4, 2, 2]
-            col_names = [
-                "record_name",
-                "atom_number",
-                "blank_1",
-                "atom_name",
-                "alt_loc",
-                "residue_name",
-                "blank_2",
-                "chain_id",
-                "residue_number",
-                "insertion",
-                "blank_3",
-                "x_coord",
-                "y_coord",
-                "z_coord",
-                "occupancy",
-                "b_factor",
-                "blank_4",
-                "segment_id",
-                "element_symbol",
-                "charge",
-            ]
-        assert sum(col_widths) == 80
-        assert len(col_widths) == len(col_names)
-        df_atom_site = pd.read_fwf(atom_site_buffer, widths=col_widths, names=col_names)
-        atom_site_buffer.close()
-
-        col_names = [col_name for col_name in col_names if "blank" not in col_name]
-        df_atom_site = df_atom_site[col_names]
-        if allow_chimera:
-            df_atom_site = _fix_chimera(df_atom_site)
-
-        if atom_site_line_nmr_model:
-            df_atom_site["nmr_model"] = atom_site_line_nmr_model
-
-        str_names = [
-            "atom_name",
-            "alt_loc",
-            "residue_name",
-            "chain_id",
-            "insertion",
-            "segment_id",
-            "element_symbol",
-        ]
-        df_atom_site[str_names] = df_atom_site[str_names].fillna("")
-
-        data["_atom_site"] = df_atom_site
+            elif line.startswith(ATOM_SITE):
+                atom_site_rows.append(
+                    _split_atom_line(
+                        line, nmr_model=nmr_model, allow_chimera=allow_chimera
+                    )
+                )
+    if num_nmr_models != n_model_lines or num_nmr_models != n_endmdl_lines:
+        warnings.warn(
+            f"The NUMMDL says {num_nmr_models} NMR models, but only {n_model_lines} found.",
+            RuntimeWarning,
+            stacklevel=2,
+        )
+    column_names = [
+        "record_name",
+        "atom_number",
+        "atom_name",
+        "alt_loc",
+        "residue_name",
+        "chain_id",
+        "residue_number",
+        "insertion",
+        "x_coord",
+        "y_coord",
+        "z_coord",
+        "occupancy",
+        "b_factor",
+        "segment_id",
+        "element_symbol",
+        "charge",
+        "nmr_model",
+    ]
+    data["_atom_site"] = pd.DataFrame(atom_site_rows, columns=column_names)
+    if n_model_lines == 0:
+        data["_atom_site"].drop(columns=["nmr_model"], inplace=True)
 
     return data
 
 
-def _fix_chimera(df_atom_site: pd.DataFrame) -> pd.DataFrame:
-    """Internal function to fix the 'record_name' and 'atom_number' columns in processing Chimera formatted PDBs.
+def _split_atom_line(
+    line: str, nmr_model: int | None = None, allow_chimera: bool = True
+) -> tuple:
+    """Internal function to parse a single line belonging to 'ATOM', 'HETATM', and 'TER' lines
 
     Args:
-        df_atom_site (pd.DataFrame): original PDB dataframe.
+        line (str): A 'ATOM', 'HETATM', and 'TER' line
+        nmr_model (int|None; defaults to None): the NMR model number for the line
+        allow_chimera (bool; defaults to True): try to parse as a Chimera-formatted PDB file.
 
     Returns:
-        pd.DataFrame: updated PDB dataframe
-    """  # noqa
-    original_record_names = list(df_atom_site.record_name)
-    record_names = [
-        record_name if record_name in ["ATOM", "TER"] else record_name + "M"
-        for record_name in original_record_names
-    ]
-    df_atom_site["record_name"] = record_names
+        tuple: parsed values
+    """
+    atom_name = line[12:16].strip()
+    alt_loc = line[16].strip()
+    chain_id = line[21]
+    residue_number = int(line[22:26].lstrip())
+    insertion = line[26].strip()
 
-    if df_atom_site["atom_number"].dtypes != "int64":
-        original_atom_numbers = list(df_atom_site.atom_number)
-        atom_numbers = [
-            int(atom_number.lstrip("M").strip())
-            for atom_number in original_atom_numbers
-        ]
-        df_atom_site["atom_number"] = atom_numbers
+    x_coord_s = line[30:38].lstrip()
+    x_coord = float(x_coord_s) if x_coord_s else None
+    y_coord_s = line[38:46].lstrip()
+    y_coord = float(y_coord_s) if y_coord_s else None
+    z_coord_s = line[46:54].lstrip()
+    z_coord = float(z_coord_s) if z_coord_s else None
 
-    return df_atom_site
+    occupancy_s = line[54:60].lstrip()
+    occupancy = float(occupancy_s) if occupancy_s else None
+    b_factor_s = line[60:66].lstrip()
+    b_factor = float(b_factor_s) if b_factor_s else None
+
+    segment_id = line[72:76].rstrip()
+    element_symbol = line[76:78].lstrip()
+    charge = line[78:80].strip()
+
+    if allow_chimera:
+        record_name = line[0:5].rstrip()
+        if record_name == "HETAT":
+            record_name = "HETATM"
+        atom_number = int(line[5:11].lstrip("M").lstrip(" "))
+        residue_name = line[17:21].strip()
+    else:
+        record_name = line[0:6].rstrip()
+        atom_number = int(line[6:11].lstrip())
+        residue_name = line[17:20].strip()
+
+    return (
+        record_name,
+        atom_number,
+        atom_name,
+        alt_loc,
+        residue_name,
+        chain_id,
+        residue_number,
+        insertion,
+        x_coord,
+        y_coord,
+        z_coord,
+        occupancy,
+        b_factor,
+        segment_id,
+        element_symbol,
+        charge,
+        nmr_model,
+    )
