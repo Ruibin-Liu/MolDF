b2ab90c8605da2e8d03e7ef0508917ba3b8ed6db
Ruibin Liu <ruibinliuphd@gmail.com>
2023-09-11 16:14:41 -0400

    doc and name change

--- pdbx2df/__init__.py
+++ pdbx2df/__init__.py
@@ -2,9 +2,19 @@
 # Author: Ruibin Liu <ruibinliuphd@gmail.com>
 # License: MIT
 # Code Repository: https://github.com/Ruibin-Liu/pdbx2df
-"""pdbx2df exported classes, functions and constants."""
+"""
+# pdbx2df - Super lightweight and fast mmCIF/PDB/MOL2 file parser into Pandas DataFrames and backwards writer.
+
+### Key features:
+
+### 1. Read a mmCIF/PDB/MOL2 file as a dictionary of Pandas DataFrame.
+
+### 2. Atom selection using dot syntax enabled by a DataFrame subclass: PDBDataFrame
+
+### 3. Write a dictionary of Pandas DataFrames into mmCIF/PDB files.
+""" # noqa
 from pdbx2df.constants import AMINO_ACIDS, ELEMENT_MASSES
-from pdbx2df.pdb_dataframe import PdbDataFrame
+from pdbx2df.pdb_dataframe import PDBDataFrame
 from pdbx2df.read_mol2 import read_mol2
 from pdbx2df.read_pdb import read_pdb
 from pdbx2df.read_pdbx import read_pdbx
@@ -15,7 +25,7 @@ from pdbx2df.write_pdbx import write_pdbx
 from .version import __version__
 
 __all__ = [
-    "PdbDataFrame",
+    "PDBDataFrame",
     "read_pdbx",
     "write_pdbx",
     "read_pdb",
