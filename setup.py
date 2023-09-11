8aa5ff3f361c13ab395c07c6a3e14d2890ed970b
Ruibin Liu <ruibinliuphd@gmail.com>
2023-09-11 16:39:11 -0400

    description

--- setup.py
+++ setup.py
@@ -8,9 +8,7 @@ from setuptools import find_packages, setup  # type: ignore
 if os.path.exists("README.md"):
     long_description = open("README.md").read()
 else:
-    long_description = (
-        "pdbx2df - A python package to parse PDBx file into Pandas DataFrames."
-    )
+    long_description = "pdbx2df - Super lightweight and fast mmCIF/PDB/MOL2 file parser into Pandas DataFrames and backwards writer."  # noqa
 
 with open("requirements.txt") as f:
     REQUIREMENTS = f.read().strip().split("\n")
@@ -24,7 +22,7 @@ setup(
     version=ver["__version__"],
     author=__author__,
     author_email="ruibinliuphd@gmail.com",
-    description="A python package to parse PDBx file into Pandas DataFrames.",
+    description="Super lightweight and fast mmCIF/PDB/MOL2 file parser into Pandas DataFrames and backwards writer.",  # noqa
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Ruibin-Liu/pdbx2df",
